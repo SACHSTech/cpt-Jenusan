660065641ca475b8c8baf8878eff8251fee6c160
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2023-01-11 10:37:22 -0500

    testing csv reading

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -2,4 +2,17 @@ package cpt;
 import java.io.*;
 
 public class Main {
+    public static void main(String[] args) {
+        String file = "src\\cpt\\0 Data.csv";
+        String line;
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while((line = br.readLine()) != null){
+                System.out.println(line.split(","));
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
