ee347a99ac5279ac114bb03ff1be42cf38bccadf
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2023-01-18 10:45:34 -0500

    streamlined files

--- src/cpt/TeamTotal.java
+++ src/cpt/TeamTotal.java
@@ -1,5 +1,27 @@
 package cpt;
+import java.util.*;
 
 public class TeamTotal {
-    
+
+    Data longList = new Data();
+    Hashtable<String, Integer> totals;
+    int x = 0;
+
+    public TeamTotal(){
+    }
+
+    public void totalUp(int start, int end){
+        totals =  new Hashtable<String, Integer>();
+        for (int i = 0; i <= 30; i++){
+            totals.put(longList.getTeam(i), 0);
+        }
+
+        while (x >= end){
+            if ()
+        }
+
+    }
+
+
+
 }
