7d58a8da85f3a20ec1e0f8c2fcdd4c9844b1d3fb
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2023-01-18 23:33:11 -0500

    drilldown kinda works but ensemble sucks

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -13,10 +13,16 @@ import java.util.ArrayList;
  
 public class Main extends Application {
     static TeamTotal TeamTotal;
-    ArrayList<Data> test;
+    static PlayerTotal playerTotal;
+    ArrayList<Data> teams;
+    ArrayList<Data> players;
     Data TeamData;
+    Data playerData;
+
+
     public static void main(String[] args) {
         TeamTotal = new TeamTotal(2020, 2015);
+        playerTotal = new PlayerTotal("Toronto Raptors");
         
         launch(args);
     }
@@ -29,40 +35,38 @@ public class Main extends Application {
     private ObservableList<Data> data;
  
     public Parent createContent() {
-        test = new ArrayList<>();
+        teams = new ArrayList<>();
         for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
             TeamData = new Data(TeamTotal.getTeamList().get(i), TeamTotal.getTable().get(TeamTotal.getTeamList().get(i)));
-            test.add(TeamData);
+            teams.add(TeamData);
         }
 
         
-        Data A, B, C, D;
-        A = new Data("A", 0);
-        B = new Data("B", 0);
-        C = new Data("C", 0);
-        D = new Data("D", 0);
+        Data A;
+        A = new Data("A", 100);
 
-        test.add(A);
-        test.add(B);
-        test.add(C);
-        test.add(D);
+        teams.add(A);
 
-        data = FXCollections.observableArrayList(test);
+        data = FXCollections.observableArrayList(teams);
         final PieChart pie = new PieChart(data);
         final String drillDownChartCss =
             getClass().getResource("0 DrilldownChart.css").toExternalForm();
         pie.getStylesheets().add(drillDownChartCss);
  
         setDrilldownData(pie, A, "a");
-        setDrilldownData(pie, B, "b");
-        setDrilldownData(pie, C, "c");
-        setDrilldownData(pie, D, "d");
         return pie;
     }
  
     private void setDrilldownData(final PieChart pie, final Data data, final String labelPrefix) {
+
+        players = new ArrayList<>();
+        for (int i = 0; i < playerTotal.getPlayerCount(); i++){
+            playerData = new Data(playerTotal.getNameList().get(i),playerTotal.getTable().get(playerTotal.getNameList().get(i)));
+            players.add(playerData);
+        }
+
         data.getNode().setOnMouseClicked((MouseEvent t) -> {
-            pie.setData(FXCollections.observableArrayList(new Data(labelPrefix + "-1", 7),new Data(labelPrefix + "-2", 2)));
+            pie.setData(FXCollections.observableArrayList(players));
         });
     }
  
--- src/cpt/PlayerTotal.java
+++ src/cpt/PlayerTotal.java
@@ -7,11 +7,13 @@ public class PlayerTotal {
     TeamTotal TeamTotal;
     Hashtable <String, Integer> playertotals;
     ArrayList<POTW> singleTeam;
+    ArrayList <String> names;
 
     public PlayerTotal(String str){
         TeamTotal = new TeamTotal(2020, 1900);
         playertotals = new Hashtable<>();
         singleTeam = new ArrayList<>();
+        names = new ArrayList<>();
         for (int i = 0; i < TeamTotal.getShortList().size(); i++){
             if ((TeamTotal.getShortList().get(i).getTeam()).equals(str)){
                 singleTeam.add(TeamTotal.getShortList().get(i));
@@ -20,17 +22,28 @@ public class PlayerTotal {
         Hashtable();
     }
 
+    // only needs to iterate once
     public void Hashtable(){
         for (int i = 0; i < singleTeam.size(); i++){
             try{
                 playertotals.put(singleTeam.get(i).getName(), playertotals.get(singleTeam.get(i).getName()) + 1);
             }catch(Exception e){
                 playertotals.put(singleTeam.get(i).getName(), 1);
+                names.add(singleTeam.get(i).getName());
             }
         }
     }
 
     public void print(){
         System.out.println(playertotals);
+    }
+    public int getPlayerCount(){
+        return playertotals.size();
+    }
+    public Hashtable <String, Integer> getTable(){
+        return playertotals;
+    }
+    public ArrayList<String> getNameList(){
+        return names;
     }
 }
