1c80f29726c9d26d3394d8648228fd37bde988ea
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2023-01-22 19:15:27 -0500

    pie chart on the main tab

--- src/cpt/TabSwitcher.java
+++ src/cpt/TabSwitcher.java
@@ -1,33 +1,75 @@
 package cpt;
-
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.*;
+import javafx.scene.Group;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import java.util.Arrays;
+
 
 public class TabSwitcher extends Application {
+
+    static TeamTotal TeamTotal;
+    static PlayerTotal playerTotal;
+
+    static Hashtable <String, ArrayList<Data>> findPlayers;
+    ArrayList<Data> teams;
+    ArrayList<Data> players;
+    Data TeamData;
+    Data playerData;
+
+    static int Start;
+    static int End;
+    static int num;
+
+    private ObservableList<Data> data;
+
     @Override
     public void start(Stage primaryStage) {
         TabPane tabPane = new TabPane();
-
+        
         Tab tab1 = new Tab("Tab 1");
-        tab1.setOnSelectionChanged(e -> {
-            if (tab1.isSelected()) {
-                tabPane.setStyle("-fx-background-color: blue;");
-            }
-        });
-
         
         Tab tab2 = new Tab("Tab 2");
-        tab2.setOnSelectionChanged(e -> {
-            if (tab2.isSelected()) {
-                tabPane.setStyle("-fx-background-color: green;");
-            }
-        });
+        
+        teams = new ArrayList<>();
+        for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
+            TeamData = new Data(TeamTotal.getTeamList().get(i) + " - " + TeamTotal.getTable().get(TeamTotal.getTeamList().get(i)), TeamTotal.getTable().get(TeamTotal.getTeamList().get(i)));
+            teams.add(TeamData);
+        }
 
+        data = FXCollections.observableArrayList(teams);
+        final PieChart pie = new PieChart(data);
+        final String drillDownChartCss =
+            getClass().getResource("0 DrilldownChart.css").toExternalForm();
+        pie.getStylesheets().add(drillDownChartCss);
+ 
+        for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
+            setDrilldownData(pie, teams.get(i), TeamTotal.getTeamList().get(i));
+        }
+        tab2.setContent(pie);
+        
         tabPane.getTabs().addAll(tab1, tab2);
 
         StackPane root = new StackPane();
@@ -41,6 +83,35 @@ public class TabSwitcher extends Application {
     }
 
     public static void main(String[] args) {
+        Start = 2020;
+        End = 1983;
+        num = 1979;
+        TeamTotal = new TeamTotal(Start, End);
+        findPlayers = new Hashtable<>(); 
+
         launch(args);
     }
+
+    private void setDrilldownData(final PieChart pie, final Data data, final String TeamName) {
+
+        for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
+            playerTotal = new PlayerTotal(TeamTotal.getTeamList().get(i), Start, End);
+
+        players = new ArrayList<>();
+
+
+        for (int x = 0; x < playerTotal.getPlayerCount(); x++){
+            playerData = new Data(playerTotal.getNameList().get(x) + " - " + playerTotal.getTable().get(playerTotal.getNameList().get(x)),playerTotal.getTable().get(playerTotal.getNameList().get(x)));
+            players.add(playerData);
+        }
+        findPlayers.put(TeamTotal.getTeamList().get(i), players);
+    }
+
+        data.getNode().setOnMouseClicked((MouseEvent t) -> {
+            System.out.println(TeamName);
+            System.out.println(findPlayers.get(TeamName));
+            pie.setData(FXCollections.observableArrayList(findPlayers.get(TeamName)));
+        });
+    }
+
 }
