94c4d85865f782ba1ea0e5f90edf4917c254056d
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2023-01-23 23:18:44 -0500

    DONE

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -75,6 +75,9 @@ public class Main extends Application {
 
     static int newValue;
 
+    /**
+     * main method to run code
+     */
     public static void main(String[] args) {
         Start = 2020;
         End = 1983;
@@ -83,26 +86,33 @@ public class Main extends Application {
         launch(args);
     }
 
+    
     @Override
+    /**
+     * start method
+     */
     public void start(Stage primaryStage) {
+        // initializes tabs
         TabPane tabPane = new TabPane();
         Tab tab1 = new Tab("Bar Chart");
 
+        // sends in data for barchart
         dataList.setItems(Bardata);
 
 
         dataList.setPrefSize(200, 200);
 
+        // constructs the barchart with given data
         HBox inputBox = new HBox();
         inputBox.setSpacing(10);
         inputBox.getChildren().addAll(yearField, addButton);
-
         VBox Barroot = new VBox();
         Barroot.setSpacing(10);
         Barroot.setPadding(new Insets(10, 10, 10, 10));
         Barroot.getChildren().addAll(bc, inputBox, dataList);
         tab1.setContent(Barroot);
 
+        // Adds new data when add button is pressed
         addButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -121,10 +131,12 @@ public class Main extends Application {
 
         bc.getData().add(series);
 
+        // second tab code
         Tab tab2 = new Tab("Pie Chart");
 
         TeamTotal = new TeamTotal(Start, End);
         
+        // puts data into arraylist of constructors
         teams = new ArrayList<>();
         for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
             TeamData = new Data(TeamTotal.getTeamList().get(i) + " - " + TeamTotal.getTable().get(TeamTotal.getTeamList().get(i)), TeamTotal.getTable().get(TeamTotal.getTeamList().get(i)));
@@ -134,6 +146,7 @@ public class Main extends Application {
         data = FXCollections.observableArrayList(teams);
         final PieChart pie = new PieChart(data);
 
+        // perams for the pie chart
         Slider slider = new Slider();
         slider.setMin(0);
         slider.setMax(100);
@@ -154,12 +167,16 @@ public class Main extends Application {
             getClass().getResource("0 DrilldownChart.css").toExternalForm();
         pie.getStylesheets().add(drillDownChartCss);
  
+        // sets drilldown data
         for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
             setDrilldownData(pie, teams.get(i), TeamTotal.getTeamList().get(i));
         }
         
+        // sets tab
         tab2.setContent(pie);
         
+
+        // peices togethere and sets final scene
         tabPane.getTabs().addAll(tab1, tab2);
 
         StackPane root = new StackPane();
@@ -172,14 +189,22 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * drilldownn data for pie chart
+     * @param pie chart
+     * @param data data for chart
+     * @param TeamName teamname string
+     */
     private void setDrilldownData(final PieChart pie, final Data data, final String TeamName) {
 
+        // cuts down to teamd
         for (int i = 0; i < TeamTotal.getTeamList().size(); i++){
             playerTotal = new PlayerTotal(TeamTotal.getTeamList().get(i), Start, End);
 
         players = new ArrayList<>();
 
 
+        // cuts down to players
         for (int x = 0; x < playerTotal.getPlayerCount(); x++){
             playerData = new Data(playerTotal.getNameList().get(x) + " - " + playerTotal.getTable().get(playerTotal.getNameList().get(x)),playerTotal.getTable().get(playerTotal.getNameList().get(x)));
             players.add(playerData);
@@ -187,6 +212,7 @@ public class Main extends Application {
         findPlayers.put(TeamTotal.getTeamList().get(i), players);
     }
 
+    // finds where users click
         data.getNode().setOnMouseClicked((MouseEvent t) -> {
             pie.setData(FXCollections.observableArrayList(findPlayers.get(TeamName)));
         });
